e10d85f113c5a0c542356fe76ecedb90020b0907
Kadet <techkadet@gmail.com>
2020-07-14 04:47:50 +0100

    before styling form

--- assets/js/home-anim.js
+++ assets/js/home-anim.js
@@ -36,28 +36,28 @@ gsap.from('.scroll', {duration:2,  opacity: 0, delay: 2.1, ease: 'back'})
 
 //initialise GSAP scroll trigger
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-function goToSection(i, anim) {
-  gsap.to(window, {
-    scrollTo: {y: i*innerHeight, autoKill: false},
-    duration: 1
-  });
+// function goToSection(i, anim) {
+//   gsap.to(window, {
+//     scrollTo: {y: i*innerHeight, autoKill: false},
+//     duration: 1
+//   });
   
-  if(anim) {
-    anim.restart();
-  }
-}
+//   if(anim) {
+//     anim.restart();
+//   }
+// }
 
-gsap.utils.toArray(".panel").forEach((panel, i) => {
-  ScrollTrigger.create({
-    trigger: panel,
-    onEnter: () => goToSection(i)
-  });
+// gsap.utils.toArray(".panel").forEach((panel, i) => {
+//   ScrollTrigger.create({
+//     trigger: panel,
+//     onEnter: () => goToSection(i)
+//   });
   
-  ScrollTrigger.create({
-    trigger: panel,
-    start: "bottom bottom",
-    onEnterBack: () => goToSection(i),
-  });
-});
+//   ScrollTrigger.create({
+//     trigger: panel,
+//     start: "bottom bottom",
+//     onEnterBack: () => goToSection(i),
+//   });
+// });
